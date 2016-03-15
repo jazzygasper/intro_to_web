e46a3d664cd574f99cb955d81697fdc64644cfe1
Patrick <p_donati@hotmail.com>
2016-03-15 10:27:36 +0000

    caaat

--- app.rb
+++ app.rb
@@ -11,3 +11,9 @@ end
 get '/name/time' do
   "its #{Time.now.strftime("%H:%M")} Patrick"
 end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+     <img src='http://bit.ly/1eze8aE'>
+   </div>"
+end
