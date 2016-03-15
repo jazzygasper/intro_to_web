7d9998c1ec58e7a0c2ac4da01e9c727d163b6e43
Jazzy <jasmina.gasper@gmail.com>
2016-03-15 10:04:11 +0000

    first commit

--- app.rb
+++ app.rb
@@ -1,15 +1,32 @@
 require 'sinatra'
 
 get '/' do
-  "hello!"
+  "Hello!"
+end
+
+get '/jazzy' do
+  "HELLO!"
 end
 
 get '/secret' do
-  "bad man"
+  "Adil is the best"
 end
 
 get '/cat' do
-   "<marquee direction='left' style='border: 3px dashed red'>
-   <img src='http://bit.ly/1eze8aE'/>
-   </marquee>"
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
 end
+
+# get '/' do
+#   "hello!"
+# end
+#
+# get '/secret' do
+#   "bad man"
+# end
+#
+# get '/cat' do
+#    "<marquee direction='left' style='border: 3px dashed red'>
+#    <img src='http://bit.ly/1eze8aE'/>
+#    </marquee>"
+# end
