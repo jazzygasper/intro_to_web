c3086d855f6aee168feb6ebae6b60e9c119e10f0
Jazzy <jasmina.gasper@gmail.com>
2016-03-15 11:17:03 +0000

    params

--- app.rb
+++ app.rb
@@ -12,7 +12,13 @@ get '/name/time' do
   "its #{Time.now.strftime("%H:%M")} Patrick"
 end
 
-get '/cat' do
+get '/random-cat' do
   @cat_name = %w(Amigo Oscar Viking).sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @cat_name = params[:name]
   erb(:index)
 end
